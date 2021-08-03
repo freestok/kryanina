165786ead705a250cb6a4b7d67c7ff796256dd5d
krayf <krayf@northpointgis.com>
2021-08-03 07:24:24 -0400

    adjusted map location starting point

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
     // set-up map and basemap
-    const map = L.map('map').setView([39.47, -10], 2);
+    const map = L.map('map').setView([39.47, 0], 2);
     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
         maxZoom: 16
