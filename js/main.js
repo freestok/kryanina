49d9c6c74ada4edeff88beec52594f307395c1da
kvrandrup <kristina.randrup@gmail.com>
2021-08-13 21:31:05 -0700

    changes from y'all and increased heading font size

--- js/main.js
+++ js/main.js
@@ -1,4 +1,41 @@
+// -----------------------------------
+// --------- GLOBAL VARIABLES --------
+let year = 2020,
+    indicator,
+    mapData,
+    colorScales = {
+        reds: chroma.scale('reds').colors(8),
+        oranges: chroma.scale('oranges').colors(8),
+        blues: chroma.scale('blues').colors(8),
+        greens: chroma.scale('greens').colors(8),
+        purples: chroma.scale('purples').colors(8)
+    };
+// -----------------------------------
+// -----------------------------------
+
 $(document).ready(() => {
+    indicator = $('input[name=flexRadioDefault]:checked')[0].id.replace('Radio','');
+    initListeners();
+    initMap();
+});
+
+
+function initListeners() {
+    // listener on radio buttons
+    $("input:radio[name=flexRadioDefault]").on("change", (e) => {
+        indicator = e.target.id.replace('Radio','');
+        mapData.eachLayer(layer => layer.setStyle(style(layer.feature)));
+    });
+
+    $('#timeSlider').on('input', (e) => {
+        let yearVal = $(e.target).val();
+        year = yearVal;
+        $('#timeLabel').text(String(yearVal));
+        mapData.eachLayer(layer => layer.setStyle(style(layer.feature)));
+    });
+}
+
+function initMap() {
     // set-up map and basemap
     const map = L.map('map').setView([39.47, 0], 2);
     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
@@ -6,17 +43,36 @@ $(document).ready(() => {
         maxZoom: 16
     }).addTo(map);
 
-
     // --------------------- add data to the map ---------------
-    // fetchJSON(`${assets}/urban_boundaries.json`).then((data) => {
-    //     L.geoJSON(data, {
-    //         style: polyStyle,
-    //         interactive: false
-    //     }).addTo(map);
-    // });
-});
+    fetchJSON('./data/vdem_15s.json').then((data) => {
+        mapData = L.geoJSON(data, {style: style}).addTo(map);;
+        console.log('mapData added to map');
+    });
+}
+
 
 async function fetchJSON(url) {
     const response = await fetch(url);
     return await response.json();
+}
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties[`${indicator}${year}`], colorScales.purples),
+        weight: .75,
+        opacity: 1,
+        color: '#191919',
+        fillOpacity: 0.7
+    };
+}
+
+function getColor(d, colorScale) {
+    return d > .9 ? colorScale[7] :
+           d > .8 ? colorScale[6] :
+           d > .7 ? colorScale[5] :
+           d > .6 ? colorScale[4] :
+           d > .5 ? colorScale[3] :
+           d > .4 ? colorScale[2] :
+           d > .3 ? colorScale[1] :
+                    colorScale[0];
 }
