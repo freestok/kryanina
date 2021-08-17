6b94533b69218668134862379d1db82188cde7bf
Ryan Cooper <ryancooper729@gmail.com>
2021-08-17 00:02:55 -0400

    recreate bar chart with c3.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 // -----------------------------------
 // --------- GLOBAL VARIABLES --------
 let year = 2020,
+    selectedCountry = null,
     indicator,
+    map,
+    mapJSON,
     mapData,
     colorScales = {
         reds: chroma.scale('reds').colors(8),
@@ -9,7 +12,8 @@ let year = 2020,
         blues: chroma.scale('blues').colors(8),
         greens: chroma.scale('greens').colors(8),
         purples: chroma.scale('purples').colors(8)
-    };
+    },
+    barChart;
 // -----------------------------------
 // -----------------------------------
 
@@ -41,7 +45,7 @@ function expandTray() {
     $(".tray").addClass("expanded box");
 
     if(($(window).width() >= 544)) {
-        $(".wrapper").css("grid-template-rows", "auto minmax(200px, 33%)");
+        $(".wrapper").css("grid-template-rows", "auto minmax(200px, 50%)");
     } else {
         $(".wrapper").css("grid-template-rows", "25% auto 25%");
     }
@@ -61,7 +65,7 @@ function resizeLayout() {
     $(window).resize(() => {
         if (($(".tray").hasClass("expanded"))) {
             if(($(window).width() >= 544)) {
-                $(".wrapper").css("grid-template-rows", "auto minmax(200px, 33%)")
+                $(".wrapper").css("grid-template-rows", "auto minmax(200px, 50%)")
             } else {
                 $(".wrapper").css("grid-template-rows", "25% auto 25%")
             }
@@ -77,7 +81,7 @@ function resizeLayout() {
 
 function initMap() {
     // set-up map and basemap
-    const map = L.map('map').setView([39.47, 0], 2);
+    map = L.map('map').setView([39.47, 0], 2);
     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
         maxZoom: 16
@@ -85,7 +89,8 @@ function initMap() {
 
     // --------------------- add data to the map ---------------
     fetchJSON('./data/vdem_15s.json').then((data) => {
-        mapData = L.geoJSON(data, {
+        mapJSON = data;
+        mapData = L.geoJSON(mapJSON, {
             style: style,
             onEachFeature: onEachFeature
         }).addTo(map);;
@@ -120,9 +125,102 @@ function getColor(d, colorScale) {
                     colorScale[0];
 }
 
+
 function createCountryReport() {
-    console.log("country clicked")
+    map.fitBounds(this.getBounds())
+    // Get selected country
+    selectedCountry = this.feature.properties.country_name;
+
+    // Retrieve attribute data for selected country
+    let selectedCountryAttributes = mapJSON.features
+    .filter(feature => feature.properties.country_name == selectedCountry)[0].properties
+    
+    // Generate array of fields for the selected year
+    let attributeYearKeys = ["el", "li", "pa", "de", "eg"].map(attr => `${attr}${year}`)
+
+    // Filter the attribute data to only the fields for the selected year
+    let attributeYearData = Object.keys(selectedCountryAttributes)
+        .filter(key => attributeYearKeys.includes(key))
+        .reduce((obj, key) => {
+            obj[key] = selectedCountryAttributes[key];
+            return obj;
+        }, {})
+    // Generate report
+    $(".report-country-name").text(selectedCountry);
+
+    // Bar Graph
+    createBarChart(attributeYearData)
+    
+    // Expand tray
     expandTray();
+
+    // Zoom to selected country
+    map.fitBounds(this.getBounds())
+}
+
+function createBarChart(data) {
+    barChart = c3.generate({
+        bindto: "#report-indicator-bar-chart",
+        size: {
+            height: $(".report-indicator-bar").height(),
+            width: $(".report-indicator-bar").width()
+        },
+        oninit: () => {
+            setTimeout(() => {
+                resizeBarChart()
+            }, 1)
+        },
+        onresized: () => {
+            resizeBarChart()
+        },
+        data: {
+            columns: [
+                ["Electoral", data[`el${year}`]],
+                ["Liberal", data[`li${year}`]],
+                ["Participatory", data[`pa${year}`]],
+                ["Deliberative", data[`de${year}`]],
+                ["Egalitarian", data[`eg${year}`]]
+            ],
+            type: "bar",
+            colors: {
+                Electoral: colorScales.reds[5],
+                Liberal: colorScales.oranges[5],
+                Participatory: colorScales.blues[5],
+                Deliberative: colorScales.greens[5],
+                Egalitarian: colorScales.purples[5],
+            }
+        },
+        bar: {
+            width: {
+                ratio: 0.95
+            }
+        },
+        axis: {
+            x: {
+                padding: {
+                    left: 0,
+                    right: 0,
+                }
+            },
+            x: {
+                show: false
+            },
+            y: {
+                min: 0,
+                max: 1,
+                padding: {
+                    top: 0,
+                    bottom: 0
+                }
+            },
+        }
+    });
+}
+function resizeBarChart() {
+    barChart.resize({
+        height: $(".report-indicator-bar").height(),
+        width: $(".report-indicator-bar").width()
+    })
 }
 
 function onEachFeature(feature, layer) {
